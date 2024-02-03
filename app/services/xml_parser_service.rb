8af6f0b2ac9aa398ccb6691b7bd4890a8cc084b4
Alberto Hernandez Ceezo <pascu216@gmail.com>
2024-02-03 10:25:58 +0100

    XML parser functioning with all tests passing

--- app/services/xml_parser_service.rb
+++ app/services/xml_parser_service.rb
@@ -1,12 +1,34 @@
 class XmlParserService
   attr_reader :data
 
-  # Service initiates with the file content of spec/fixtures/data.xml
   def initialize(data)
     @data = data
   end
 
   def parse
-    # TODO: Please write parsing method to parse the contents of spec/fixtures/data.xml into Listing records.
+    listings_payload = Nokogiri::XML(data).xpath("//listing").map { listing_payload(_1) }
+    Listing.insert_all!(listings_payload)
+  end
+
+  private
+
+  def listing_payload(xml_data_item)
+    {
+      start_time: to_utc_timestamp(date(xml_data_item), xml_data_item.attribute("start").value, time_zone(xml_data_item)),
+      end_time: to_utc_timestamp(date(xml_data_item), xml_data_item.attribute("stop").value, time_zone(xml_data_item)),
+      title: xml_data_item.xpath("title").text,
+      description: xml_data_item.xpath("description").text,
+      episode_number: xml_data_item.xpath("episode").text,
+      season_number: xml_data_item.xpath("episode").attribute("season")&.value,
+      image_url: xml_data_item.xpath("image").text
+    }
+  end
+  
+  def date(xml_data_item) = xml_data_item.attribute("date").value.to_date
+
+  def time_zone(xml_data_item) = xml_data_item.attribute("tz").value
+
+  def to_utc_timestamp(date, time, time_zone)
+    ActiveSupport::TimeZone[time_zone].parse("#{date} #{time}").utc
   end
 end
